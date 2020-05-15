7349dfbc1ab35c1d758f8ec187bec76252f57af9
Sangeeth Raj <47982022+SangeethRaajA@users.noreply.github.com>
2020-05-15 12:21:20 +0530

    Add files via upload
    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/',(req,res) =>{
+    const articles = [{
+        title : 'test article',
+        createdAt : Date.now(),
+        description : 'test desc'
+    }]
+    res.render('index', { articles: articles })
+})
+app.listen(5003)
