e8dd319a81114642638b565014665c60d989a05f
Sangeeth Raj <47982022+SangeethRaajA@users.noreply.github.com>
2020-05-18 15:36:06 +0530

    display new article commit

--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const Article = require('./../models/article');
 
-const Router = express.Router()
+const router = express.Router()
 
-Router.get('/new', (req,res) => {
+router.get('/new', (req,res) => {
         res.render('articles/new', { article: new Article() })
 })
 
-// router.get('/:id', (req,res) => {
-//   const article = Article.findById(req.params.id)
+router.get('/:id', async  (req, res) => {
+  const article = await Article.findById(req.params.id)
+  if(article == null) res.redirect('/')
+  res.render('articles/show', { article: article })
+})
+
+// router.get(`/:id`, async  (req, res) => {
+//   const article = await Article.findById(req.params.id)
+//   if(article == null) res.redirect('/')
+//   res.render('articles/show', { article: article })
 //   res.send(req.params.id)
 // })
-
-Router.post('/', async (req, res) =>{
-  const article = new Article({
+ 
+router.post('/', async (req, res) =>{
+  let article = new Article({
     title : req.body.title,
     description : req.body.description,
     markdown : req.body.markdown
@@ -21,12 +29,12 @@ Router.post('/', async (req, res) =>{
 
   try{
     article = await article.save()
-    res.redirect('/article/${article.id}')
+    res.redirect(`/articles/${article.id}`)
   }catch(e){
-    // console.log(e)
+    console.log(e);
     res.render('articles/new', { article: article })
   }
   
 })
 
-module.exports = Router
+module.exports = router
